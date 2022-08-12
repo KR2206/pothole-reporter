3b1fe604f8560785c568b1e69149020941f14c5a
KR2206 <bhutoray123@gmail.com>
2022-08-13 00:13:05 +0530

    Added mail.ejs

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set("view engine",'ejs');
 const arr = ["gochi", "gang"]
 app.get("/",function(req,res){
     //res.render("tweet",{handles:arr, link:"https%3A%2F%2Fwww.google.com%2Fmaps%2Fplace%2FFortis%2BHospital.%2F%4022.5499828%2C88.3785368%2C15z%2Fdata%3D%214m5%213m4%211s0x3a0277f15cc181b9%3A0xf6b86895953bf55c%218m2%213d22.5439494%214d88.3806109"});
-    res.render("test",{api:process.env.MAPS_API});
+    res.render("mail",{api:process.env.MAPS_API});
 })
 
 app.listen(3000,function(){
