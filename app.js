2251511f6d316edd49e800bac331c7981694daf4
KR2206 <bhutoray123@gmail.com>
2022-08-13 15:35:52 +0530

    Return coords done

--- app.js
+++ app.js
@@ -11,14 +11,17 @@ app.use(express.static(__dirname + '/public'));
 app.set("view engine",'ejs');
 
 
-const arr = ["gochi", "gang"]
-const coords = {
+let arr = []
+let coords = {
     lat:0,
     long:0
 }
 
 app.get("/",function(req,res){
     //res.render("tweet",{handles:arr, link:"https%3A%2F%2Fwww.google.com%2Fmaps%2Fplace%2FFortis%2BHospital.%2F%4022.5499828%2C88.3785368%2C15z%2Fdata%3D%214m5%213m4%211s0x3a0277f15cc181b9%3A0xf6b86895953bf55c%218m2%213d22.5439494%214d88.3806109"});
+    arr=[];
+    coords.lat=0;
+    coords.long=0;
     res.render("home");
 })
 app.get("/adjust",function(req,res){
@@ -33,7 +36,6 @@ app.post("/",function(req,res){
 app.post("/adjust",function(req,res){
     coords.lat = req.body.lat;
     coords.long = req.body.long;
-    console.log(coords);
 })
 
 app.listen(3000,function(){
