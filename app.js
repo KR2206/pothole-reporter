e4609681ab1e95eee8206d79bdafde5d4711708a
KR2206 <bhutoray123@gmail.com>
2022-08-17 23:55:27 +0530

    Add nodemailer and mongodb stuff

--- app.js
+++ app.js
@@ -4,19 +4,30 @@ const app = express();
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const axios = require('axios').default;
-
+const mongoose = require("mongoose");
+const nodeoutlook = require("nodejs-nodemailer-outlook");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 const stateList = ["Andaman and Nicobar Islands","Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chandigarh","Chhattisgarh","Dadra and Nagar Haveli and Daman and Diu","Delhi","Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Ladakh","Lakshadweep","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Puducherry","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal"];
+
+const stateSchema = new mongoose.Schema({
+  state: String,
+  emails: [String],
+  handles: [String]
+})
+const State = mongoose.model("state",stateSchema);
+
 let handles = ["multiversus","amongus"];
 let mails = ["a@b.com","b@c.com"];
 let coords = {
   lat: 0,
   long: 0,
 };
-let state="Telangana";
+let chosenState="Telangana";
+const password = process.env.PASSWORD;
+let mongoURL = process.env.MONGOURL
 
 //--------------GET REQUESTS---------------------
 app.get("/", function (req, res) {
@@ -24,7 +35,7 @@ app.get("/", function (req, res) {
   handles = [];
   coords.lat = 0;
   coords.long = 0;
-  state="";
+  chosenState="";
   res.render("home");
 });
 
@@ -33,7 +44,27 @@ app.get("/adjust", function (req, res) {
 });
 
 app.get("/confirm",function(req,res){
-  res.render("confirm",{state:state,mails:mails});
+  mongoose.connect(mongoURL,{ useNewUrlParser: true });
+
+    
+
+    State.findOne({state: "test"},function(err,doc){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(doc===null){
+                console.log("Empty");
+            }
+            else{
+                console.log(doc);
+                mails = doc.emails;
+                handles = doc.handles;
+                res.render("confirm",{state:chosenState,mails:mails});
+            }
+        }
+    })
+  
 })
 
 app.get("/err",function(req,res){
@@ -69,7 +100,7 @@ app.post("/adjust", function (req, res) {
         if(typeof(response.data.address.state)==="string"){
           console.log(response.data.address.state);
           console.log(typeof(response.data.address.state));
-          state = response.data.address.state;
+          chosenState = response.data.address.state;
           res.redirect("/confirm");
         }
         else{
@@ -86,12 +117,25 @@ app.post("/adjust", function (req, res) {
 app.post("/confirm",function(req,res){
   if(req.body.btn==="yes"){
     console.log("Amongus");
+    nodeoutlook.sendEmail({
+      auth: {
+          user: "potholereporter@outlook.com",
+          pass: password
+      },
+      from: "potholereporter@outlook.com",
+      to: mails,
+      subject: "Test",
+      text: "Hello\nnice",
+      onError: (e) => console.log(e),
+      onSuccess: (i) => console.log(i)
+
+  })
   }
   res.redirect("/tweet");
 })
 
 app.post("/err",function(req,res){
-  state = req.body.state;
+  chosenState = req.body.state;
   res.redirect("/confirm");
 })
 
